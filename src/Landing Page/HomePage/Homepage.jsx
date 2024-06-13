b7fe2f775ffa431899ebd740fed4bcd843709c89
Jephthah Ndukwe <jephthahndukwe@gmail.com>
2024-06-13 20:05:58 +0100

    new update

--- src/Landing Page/HomePage/Homepage.jsx
+++ src/Landing Page/HomePage/Homepage.jsx
@@ -9,6 +9,9 @@ import image from '../../assets/image 3.png'
 import image2 from '../../assets/image 4.png'
 import doctor from '../../assets/doctor.png'
 import img2 from '../../assets/Rectangle 7.png'
+import { FaStar } from "react-icons/fa6";
+import point from '../../assets/Bulleted point.png'
+import highlight from '../../assets/highlight.png'
 
 const Homepage = () => {
   return (
@@ -76,6 +79,72 @@ const Homepage = () => {
             </div>
         </div>
 
+        <div className='flex items-center justify-between px-[60px] mt-[8rem] pb-[8rem]'>
+            <div className='w-[50%]'>
+                <h1 className='text-[#000] text-[96px] font-[700] font-Inter leading-[116.18px]'>4.8</h1>
+                <div className='flex items-center gap-[5px] mt-[3rem] text-[#EDC55F]'>
+                <h3><FaStar/></h3>
+                <h3><FaStar/></h3>
+                <h3><FaStar/></h3>
+                <h3><FaStar/></h3>
+                <h3><FaStar/></h3>
+                </div>
+                <h3 className='text-[#000] text-[20px] font-[700] font-Inter leading-[24px] mt-[3rem]'>2,394 Ratings</h3>
+                <h3 className='text-[#000] text-[20px] font-[700] font-Inter leading-[24px] mt-[1.5rem]'>Google Reviews</h3>
+            </div>
+            <div className='w-[50%]'>
+                <h1 className='text-[#000] text-[96px] font-[700] font-Inter leading-[116.18px]'>A +</h1>
+                <div className='flex items-center gap-[5px] mt-[3rem] text-[#EDC55F]'>
+                <h3><FaStar/></h3>
+                <h3><FaStar/></h3>
+                <h3><FaStar/></h3>
+                <h3><FaStar/></h3>
+                <h3><FaStar/></h3>
+                </div>
+                <h3 className='text-[#000] text-[20px] font-[700] font-Inter leading-[24px] mt-[3rem]'>125 Reviews</h3>
+                <h3 className='text-[#000] text-[20px] font-[700] font-Inter leading-[24px] mt-[1.5rem]'>BBB Rating</h3>
+            </div>
+            <div className='w-[50%]'>
+                <h1 className='text-[#050505] text-[41px] font-[700] font-Inter leading-[58.09px] '>Trusted by numerous Medical patients</h1>
+                <h3 className='text-[#17B978] text-[24px] leading-[29px] font-[700] font-Inter mt-[4rem]'>Jessica Simon</h3>
+                <p className='text-[#000] text-[18px] font-[400] leading-[24px] font-Inter w-[593px] mt-[2rem]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur ex purus. Aenean convallis velit vel nisi tempus pulvinar. Morbi eu</p>
+            </div>
+        </div>
+
+        <div className='bg-[#E2F3F5] px-[40px] pt-[3rem]'>
+            <div className='flex items-center justify-between px-[80px]'>
+                <h1 className='text-[#000] text-[40px] font-[500]'>Our Services</h1>
+                <button className='w-[325px] py-[10px] px-[10px] text-center rounded-[4px] bg-[#17B978] text-[17px] text-[#fff] font-semibold font-Inter'>View All Services</button>
+            </div>
+            <center>
+                <div className="flex gap-[1.5rem] mt-5">
+                  <hr className="w-[40%] mt-3 bg-[#000] h-[2px]" />
+                  <p className='text-[18px] text-[#000] font-light font-Inter text-center'>Highlights & Advantages</p>
+                  <hr className="w-[40%] mt-3 bg-[#000] h-[2px]" />
+                </div>
+            </center>
+            <div className='mt-[4rem] flex items-center gap-[4rem] pb-[5rem]'>
+                <div>
+                    <h1 className='text-[#000] text-[30px] font-medium font-Inter'>Highlights</h1>
+                    <p className='text-[#000] text-[18px] font-[300] font-Inter text-start mt-[3rem] flex items-start gap-[8px]'><img src={point} className='mt-[4px]' /> Secure database for storing medical records</p>
+                    <p className='text-[#000] text-[18px] font-[300] font-Inter text-start mt-[1rem] flex items-start gap-[8px]'><img src={point} className='mt-[4px]' /> Notification system for reminding users of upcoming appointments</p>
+                    <p className='text-[#000] text-[18px] font-[300] font-Inter text-start mt-[1rem] flex items-start gap-[8px]'><img src={point} className='mt-[4px]' /> Integration with Google Maps for locating nearby healthcare providers</p>
+                    <p className='text-[#000] text-[18px] font-[300] font-Inter text-start mt-[1rem] flex items-start gap-[8px]'><img src={point} className='mt-[4px]' /> Ability to message doctors directly through the app for non-emergency inquiries</p>
+                    <p className='text-[#000] text-[18px] font-[300] font-Inter text-start mt-[1rem] flex items-start gap-[8px]'><img src={point} className='mt-[4px]' /> Ability to request prescription refills through the app.</p>
+                </div>
+                <div>
+                    <h1 className='text-[#000] text-[30px] font-medium font-Inter'>Advantages</h1>
+                    <p className='text-[#000] text-[18px] font-[300] font-Inter text-start mt-[2rem] flex items-start gap-[8px]'><img src={point} className='mt-[4px]' /> Convenient access to medical services from the comfort of home</p>
+                    <p className='text-[#000] text-[18px] font-[300] font-Inter text-start mt-[1rem] flex items-start gap-[8px]'><img src={point} className='mt-[4px]' /> Easy appointment scheduling with a wide range of doctors and specialists</p>
+                    <p className='text-[#000] text-[18px] font-[300] font-Inter text-start mt-[1rem] flex items-start gap-[8px]'><img src={point} className='mt-[4px]' /> A personalized medical record that is easily accessible and up-to-date</p>
+                    <p className='text-[#000] text-[18px] font-[300] font-Inter text-start mt-[1rem] flex items-start gap-[8px]'><img src={point} className='mt-[4px]' /> Improved medication adherence through customizable reminders</p>
+                    <p className='text-[#000] text-[18px] font-[300] font-Inter text-start mt-[1rem] flex items-start gap-[8px]'><img src={point} className='mt-[4px]' /> Time-saving features that reduce wait times and streamline the healthcare experience</p>
+                </div>
+                <div>
+                    <img src={highlight} className='w-[600px]' />
+                </div>
+            </div>
+        </div>
         
     </div>
   )
