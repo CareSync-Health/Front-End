c7bc6cf02c58ae910cee321e7c1377199fd26915
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-25 23:41:51 +0100

    auto-commit

--- src/Redux/Actions/DoctorActions.js
+++ src/Redux/Actions/DoctorActions.js
@@ -8,21 +8,21 @@ const url = config.liveUrl
 
 export const doctor_register = (body, navigate) => async (dispatch) => {
 	try {
-		dispatch({ type: types.ADMIN_AUTH_REQUEST })
+		dispatch({ type: types.DOCTOR_AUTH_REQUEST })
 
-		const { data } = await axios.post(`${url}/admin/`, body)
+		const { data } = await axios.post(`${url}/DOCTOR/`, body)
 		if (data.status === 'ok') {
-			dispatch({ type: types.ADMIN_AUTH_SUCCESS, payload: data.data })
+			dispatch({ type: types.DOCTOR_AUTH_SUCCESS, payload: data.data })
 			toast.success(data.message, {
 				position: 'top-right',
 			})
-			navigate('/admindashboard');
+			navigate('/about');
 		}
 	} catch (error) {
 		const message = error.response
 			? error.response.data.message
 			: 'Something went wrong'
 
-		dispatch({ type: types.ADMIN_AUTH_FAIL, payload: message })
+		dispatch({ type: types.DOCTOR_AUTH_FAIL, payload: message })
 	}
 }
