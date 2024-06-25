d15766e8bfdfcad48b74a6ffefe29bcf64ccd649
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-25 23:14:50 +0100

    auto-commit

--- src/Landing Page/Contact/Contact.jsx
+++ src/Landing Page/Contact/Contact.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from '../Navbar'
+import Footer from '../Footer'
 
 const Contact = () => {
   return (
     <>
       <Navbar/>
-      
+      <Footer/>
     </>
   )
 }
