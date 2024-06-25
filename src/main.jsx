e2bea6422b119813c6f2974a89169a118a980cc3
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-26 00:04:33 +0100

    auto-commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { Provider } from 'react-redux'
+import configureStore from './Redux/Store'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
+
+const store = configureStore()
+const persistor = persistStore(store)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <PersistGate persistor={persistor}></PersistGate>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  
+    <PersistGate persistor={persistor}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </PersistGate>
+  </Provider>
 )
