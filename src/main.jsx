fef2e627b404bd06e5efbc917fde15239bfe632a
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-26 00:04:21 +0100

    auto-commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,10 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <PersistGate persistor={persistor}></PersistGate>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  
 )
