494130f38b46fe98bedc4c586eae7f24d572dbd4
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-25 23:44:08 +0100

    auto-commit

--- src/Landing Page/Auth/Doctor/Signup.jsx
+++ src/Landing Page/Auth/Doctor/Signup.jsx
@@ -6,7 +6,10 @@ import logo from '../../../assets/logo.png'
 
 const Signup = () => {
     const submitHandler = ()=>{
-        const
+        const body = {
+
+        }
+        dispatch
     }
   return (
     <div style={{ backgroundImage: `url(${img})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }} className='px-[50px] py-[40px]'>
