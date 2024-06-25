244d6006e7fa2b914e2ea0868098561a60dfa80c
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-25 23:37:50 +0100

    auto-commit

--- src/Redux/Reducers/PatientReducers.js
+++ src/Redux/Reducers/PatientReducers.js
@@ -2,13 +2,13 @@ import * as types from "../Types"
 
 export const patientAuthReducer = (state = {}, action) => {
 	switch (action.type) {
-		case types.ADMIN_AUTH_REQUEST:
+		case types.PATIENT_AUTH_REQUEST:
 			return { loading: true }
-		case types.ADMIN_AUTH_SUCCESS:
-			return { loading: false, adminDetail: action.payload }
-		case types.ADMIN_AUTH_FAIL:
+		case types.PATIENT_AUTH_SUCCESS:
+			return { loading: false, PATIENTDetail: action.payload }
+		case types.PATIENT_AUTH_FAIL:
 			return { loading: false, error: action.payload }
-		case types.ADMIN_AUTH_LOGOUT:
+		case types.PATIENT_AUTH_LOGOUT:
 			return {}
 		default:
 			return state
