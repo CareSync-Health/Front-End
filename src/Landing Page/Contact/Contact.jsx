5eff8940bc7e1aed42f9d92132fea8d7cc781fa9
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-26 01:55:52 +0100

    auto-commit

--- src/Landing Page/Contact/Contact.jsx
+++ src/Landing Page/Contact/Contact.jsx
@@ -13,12 +13,22 @@ const Contact = () => {
       <main className='max-w-7xl w-full mt-12'>
         <section className='w-1/2 px-12'>
           <h1 className='text-6xl font-bold'>Have Questions?</h1>
-          <p className='mt-12 text-xl'>Please feel free to call or email us,<br/>or use our contact form to get in touch with us.<br/>We look forward to hearing from you!</p>
+          <p className='mt-12 text-xl'>Please feel free to call or email us,<br />or use our contact form to get in touch with us.<br />We look forward to hearing from you!</p>
           <p className='text-2xl font-semibold mt-12 mb-5'>Emergency? Call Us:</p>
           <a href="" className='text-xl'>+123 45 678 910</a>
           <p className='text-2xl font-semibold mt-12 mb-5'>Send Us Mail</p>
           <a href="" className='text-xl underline underline-offset-[5px]'>caresync@healthcare.com</a>
           <p className='text-2xl font-semibold mt-12 mb-5'>Find the Studio</p>
+          <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "100vh", width: "100%" }}>
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[51.505, -0.09]}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
         </section>
         <section></section>
       </main>
