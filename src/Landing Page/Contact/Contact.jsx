886f83636ec5df251cf6781b1d1ff216b7a6202b
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-26 01:42:18 +0100

    auto-commit

--- src/Landing Page/Contact/Contact.jsx
+++ src/Landing Page/Contact/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
         <section className='w-1/2 px-12'>
           <h1 className='text-6xl font-bold'>Have Questions?</h1>
           <p className='mt-12 text-xl'>Please feel free to call or email us,<br/>or use our contact form to get in touch with us.<br/>We look forward to hearing from you!</p>
-          <p className='text-lg font-semibold'>Emergency? Call Us:</p>
+          <p className='text-xl font-semibold'>Emergency? Call Us:</p>
         </section>
         <section></section>
       </main>
