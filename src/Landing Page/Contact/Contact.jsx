12ac671413c7ee6f6880e728725f1b009e2c1a2d
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-26 01:36:08 +0100

    auto-commit

--- src/Landing Page/Contact/Contact.jsx
+++ src/Landing Page/Contact/Contact.jsx
@@ -5,7 +5,7 @@ import Footer from '../Footer'
 const Contact = () => {
   return (
     <>
-      <div className='shadow-xl shadow-black'>
+      <div className='shadow-md shadow-black'>
         <Navbar />
       </div>
       <main className='max-w-7xl w-full'>
