f3b46d5424a724b3269399ebdb582c699435f950
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-26 01:37:09 +0100

    auto-commit

--- src/Landing Page/Contact/Contact.jsx
+++ src/Landing Page/Contact/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
       <div className='shadow-lg shadow-[grey]'>
         <Navbar />
       </div>
-      <main className='max-w-7xl w-full'>
+      <main className='max-w-7xl w-full mt-12'>
         <section className='w-1/2 px-12'>
           <h1 className='text-6xl font-bold'>Have Questions?</h1>
         </section>
