633d4a81ed33c0c4251dc3f1f4fc642d57aa6054
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-26 01:40:09 +0100

    auto-commit

--- src/Landing Page/Contact/Contact.jsx
+++ src/Landing Page/Contact/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
       <main className='max-w-7xl w-full mt-12'>
         <section className='w-1/2 px-12'>
           <h1 className='text-6xl font-bold'>Have Questions?</h1>
-          <p>Please feel free to call or email us,<br/>or use our contact form to get in touch with us.<br/>We look forward to hearing from you!</p>
+          <p className='mt-5'>Please feel free to call or email us,<br/>or use our contact form to get in touch with us.<br/>We look forward to hearing from you!</p>
         </section>
         <section></section>
       </main>
