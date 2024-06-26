1c9ba1896bdf4b54b106f85a0d175992a874858f
Jephthah Ndukwe <jephthahndukwe@gmail.com>
2024-06-26 18:17:02 +0100

    new update

--- src/Doctor Dashboard/Components/Navbar.jsx
+++ src/Doctor Dashboard/Components/Navbar.jsx
@@ -45,7 +45,6 @@ const Navbar = ({ messageCount, notificationCount }) => {
             </span>
             )}
           </div>
-          
             <div className='flex items-center relative'>
               <img src={Notification} className='w-25' alt='Notification' />
               {notificationCount != null && (
--- src/Doctor Dashboard/DoctorDashboard.jsx
+++ src/Doctor Dashboard/DoctorDashboard.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Sidebar from './Components/Sidebar'
 import Navbar from './Components/Navbar'
+import firstaid from '../assets/first aid.png'
+import crutch from '../assets/crutch.png'
+import operationtheater from '../assets/operation theater.png'
+import bank from '../assets/bank.png'
 
 const DoctorDashboard = () => {
   return (
@@ -8,9 +12,36 @@ const DoctorDashboard = () => {
         <Sidebar/>
       <div className='w-full'>
         <Navbar messageCount={5} notificationCount={12}/>
-        <div>
-          <div className='px-[30px]'>
-            
+        <div className='px-[30px]'>
+          <div className='flex items-center mt-[1rem] gap-[2rem]'>
+            <div className='bg-white w-[250px] h-[88px] rounded-[10px] px-[15px] flex items-center justify-start gap-[1rem]'>
+              <img src={firstaid} />
+              <div>
+                <h1 className='text-[#25282B] text-[14px] font-Inter font-bold leading-[20px] text-start'>Appointments</h1>
+                <h2 className='text-[#22D1EE] text-[16px] font-Inter font-normal leading-[24px] text-start mt-[5px]'>213</h2>
+              </div>
+            </div>
+            <div className='bg-white w-[250px] h-[88px] rounded-[10px] px-[15px] flex items-center justify-start gap-[1rem]'>
+              <img src={crutch} />
+              <div>
+                <h1 className='text-[#25282B] text-[14px] font-Inter font-bold leading-[20px] text-start'>New Patients</h1>
+                <h2 className='text-[#22D1EE] text-[16px] font-Inter font-normal leading-[24px] text-start mt-[5px]'>104</h2>
+              </div>
+            </div>
+            <div className='bg-white w-[250px] h-[88px] rounded-[10px] px-[15px] flex items-center justify-start gap-[1rem]'>
+              <img src={operationtheater} />
+              <div>
+                <h1 className='text-[#25282B] text-[14px] font-Inter font-bold leading-[20px] text-start'>Operations</h1>
+                <h2 className='text-[#22D1EE] text-[16px] font-Inter font-normal leading-[24px] text-start mt-[5px]'>24</h2>
+              </div>
+            </div>
+            <div className='bg-white w-[250px] h-[88px] rounded-[10px] px-[15px] flex items-center justify-start gap-[1rem]'>
+              <img src={bank} />
+              <div>
+                <h1 className='text-[#25282B] text-[14px] font-Inter font-bold leading-[20px] text-start'>Your Earnings</h1>
+                <h2 className='text-[#22D1EE] text-[16px] font-Inter font-normal leading-[24px] text-start mt-[5px]'>$ 12,174</h2>
+              </div>
+            </div>
           </div>
         </div>
       </div>
