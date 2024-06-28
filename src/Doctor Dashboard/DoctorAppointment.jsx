4cd6b61daa82d643712ad9b25726a961a7412a0b
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-28 21:20:29 +0100

    auto-commit

--- src/Doctor Dashboard/DoctorAppointment.jsx
+++ src/Doctor Dashboard/DoctorAppointment.jsx
@@ -7,9 +7,9 @@ import avatar from '../assets/avatar.png';
 const DoctorAppointment = () => {
   return (
     <div className='flex'>
-        <Sidebar/>
+      <Sidebar />
       <div className='flex-1 lg:h-[99.9vh]  xs:h-[85vh]  overflow-y-auto bg-[#E2F3F5]' style={{ msOverflowStyle: 'none', scrollbarWidth: 'none' }}>
-        <Navbar messageCount={5} notificationCount={12}/>
+        <Navbar messageCount={5} notificationCount={12} />
         <div>
           {/* starting coding from here don't touch any other thing from the navbar and sidebar please. if you touch am... YOU DIE 🔪😤 - i get coconut head 💀*/}
           <div className='px-[30px]'>
