47793732831d6307302613355dab584137952569
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-28 20:37:04 +0100

    auto-commit

--- src/Doctor Dashboard/DoctorAppointment.jsx
+++ src/Doctor Dashboard/DoctorAppointment.jsx
@@ -35,7 +35,7 @@ const DoctorAppointment = () => {
                     <td>09:15-09:45am</td>
                     <td>Dr. Jacob Jones</td>
                     <td>Mumps Stage II</td>
-                    <td></td>
+                    <td><a href=""></a></td>
                   </tr>
                 </tbody>
               </table>
