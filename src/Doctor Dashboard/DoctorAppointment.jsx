b477aa3a03ef0b29440b605e1da4f37d54dccea6
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-28 20:57:27 +0100

    auto-commit

--- src/Doctor Dashboard/DoctorAppointment.jsx
+++ src/Doctor Dashboard/DoctorAppointment.jsx
@@ -15,8 +15,8 @@ const DoctorAppointment = () => {
           <div className='px-[30px]'>
             <h1 className='text-4xl font-semibold mt-5'>Appointments</h1>
             <div className='bg-white rounded-xl w-full'>
-              <table>
-                <thead>
+              <table className='w-full'>
+                <thead className='w-full'>
                   <tr className='w-full'>
                     <th className='align-middle'>Name</th>
                     <th>Email</th>
