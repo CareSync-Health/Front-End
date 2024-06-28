3138db01653293f2dd11763434216561646083b8
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-28 21:18:56 +0100

    auto-commit

--- src/Doctor Dashboard/DoctorAppointment.jsx
+++ src/Doctor Dashboard/DoctorAppointment.jsx
@@ -14,7 +14,7 @@ const DoctorAppointment = () => {
           {/* starting coding from here don't touch any other thing from the navbar and sidebar please. if you touch am... YOU DIE 🔪😤 - i get coconut head 💀*/}
           <div className='px-[30px]'>
             <h1 className='text-4xl font-semibold mt-5'>Appointments</h1>
-            <div className='bg-white rounded-xl w-full mt-7'>
+            <div className='bg-white rounded-lg w-full mt-6'>
               <table className='w-full text-left text-xs'>
                 <thead className=''>
                   <tr className='border-b'>
