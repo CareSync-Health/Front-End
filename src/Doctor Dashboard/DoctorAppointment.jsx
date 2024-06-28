97245d3b4e91b368e938e33b46dc49939e9a5fce
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-28 20:38:47 +0100

    auto-commit

--- src/Doctor Dashboard/DoctorAppointment.jsx
+++ src/Doctor Dashboard/DoctorAppointment.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Sidebar from './Components/Sidebar'
 import Navbar from './Components/Navbar'
+import { FaPencilAlt, FaTrashAlt } from 'react-icons/fa';
+import avatar from '../assets/avatar.png';
 
 const DoctorAppointment = () => {
   return (
