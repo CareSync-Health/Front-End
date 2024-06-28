e99753a1487dc63eae95bb374d8d37627a4e290d
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-28 22:55:35 +0100

    auto-commit

--- src/Doctor Dashboard/DoctorAppointment.jsx
+++ src/Doctor Dashboard/DoctorAppointment.jsx
@@ -358,9 +358,9 @@ const DoctorAppointment = () => {
         <Navbar messageCount={5} notificationCount={12} />
         <div>
           {/* starting coding from here don't touch any other thing from the navbar and sidebar please. if you touch am... YOU DIE 🔪😤 - i get coconut head 💀*/}
-          <div className='px-[30px]'>
+          <div className='px-[30px] mb-5'>
             <h1 className='text-4xl font-semibold mt-5'>Appointments</h1>
-            <section className='bg-white rounded-md w-full my-5 overflow-auto'>
+            <section className='bg-white rounded-md w-full mt-5 overflow-auto'>
               <table className='w-full text-left text-xs'>
                 <thead className=''>
                   <tr className='border-b'>
