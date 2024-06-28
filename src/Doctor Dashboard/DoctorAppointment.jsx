0a88fcda439c8b5cfa58a84de3abb9f1621589c9
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-28 21:08:26 +0100

    auto-commit

--- src/Doctor Dashboard/DoctorAppointment.jsx
+++ src/Doctor Dashboard/DoctorAppointment.jsx
@@ -19,11 +19,11 @@ const DoctorAppointment = () => {
                 <thead className=''>
                   <tr className='border-b'>
                     <th className='font-medium p-4 align-middle'>Name</th>
-                    <th className='font-medium'>Email</th>
-                    <th className='font-medium'>Date</th>
-                    <th className='font-medium'>Visit Time</th>
-                    <th className='font-medium'>Doctor</th>
-                    <th className='font-medium'>Conditions</th>
+                    <th className='font-medium p-4 align-middle'>Email</th>
+                    <th className='font-medium p-4 align-middle'>Date</th>
+                    <th className='font-medium p-4 align-middle'>Visit Time</th>
+                    <th className='font-medium p-4 align-middle'>Doctor</th>
+                    <th className='font-medium p-4 align-middle'>Conditions</th>
                   </tr>
                 </thead>
                 <tbody>
