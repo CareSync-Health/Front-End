d2c37cc8bff079eade5fd2e9188e72fdea897f40
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-28 22:11:14 +0100

    auto-commit

--- src/Doctor Dashboard/DoctorAppointment.jsx
+++ src/Doctor Dashboard/DoctorAppointment.jsx
@@ -42,17 +42,17 @@ const DoctorAppointment = () => {
                 </tbody>
               </table>
               <div className='p-5 flex gap-1'>
-                <a onClick={()=> page > 1 ? setPage(page - 1) : ""} className={`border p-[0.45rem] rounded-md ${page > 1 ? "cursor-pointer" : "cursor-not-allowed"}`}>
+                <a onClick={() => page > 1 ? setPage(page - 1) : ""} className={`border p-[0.45rem] rounded-md ${page > 1 ? "cursor-pointer" : "cursor-not-allowed"}`}>
                   <svg className='w-3' viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <g opacity="0.7">
                       <path d="M9.71199 2.93311L4.91199 7.73311L9.71199 12.5331L11.088 11.1731L7.64799 7.73311L11.088 4.29311L9.71199 2.93311Z" fill={`${page > 1 ? "#A6FFF2" : "#E8E8E8"}`} />
                     </g>
                   </svg>
                 </a>
-                {Array.from({ length: 5 }).map((num, index, arr)=>(
-                  <a key={index} onClick={()=> setPage(index + 1)} className={`cursor-pointer text-xs leading-none p-[0.45rem] rounded-md ${page === index + 1 ? "text-white bg-[#17B978]" : "text-black border"}`}><span className='leading-none min-w-3 inline-block text-center'>{index + 1}</span></a>
+                {Array.from({ length: 5 }).map((num, index, arr) => (
+                  <a key={index} onClick={() => setPage(index + 1)} className={`cursor-pointer text-xs leading-none p-[0.45rem] rounded-md ${page === index + 1 ? "text-white bg-[#17B978]" : "text-black border"}`}><span className='leading-none min-w-3 inline-block text-center'>{index + 1}</span></a>
                 ))}
-                <a onClick={()=> page < 5 ? setPage(page +1) : ""} className='border p-[0.45rem] rounded-md'>
+                <a onClick={() => page < 5 ? setPage(page + 1) : ""} className='border p-[0.45rem] rounded-md'>
                   <svg className='w-3' viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M6.288 12.5331L11.088 7.73311L6.288 2.93311L4.928 4.29311L8.352 7.73311L4.912 11.1731L6.288 12.5331Z" fill={`${page < 5 ? "#A6FFF2" : "#E8E8E8"}`} />
                   </svg>
