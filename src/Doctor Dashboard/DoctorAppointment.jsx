0b4cf73a6d3c562ca6a16d27d440c946e306c2da
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-28 21:39:47 +0100

    auto-commit

--- src/Doctor Dashboard/DoctorAppointment.jsx
+++ src/Doctor Dashboard/DoctorAppointment.jsx
@@ -48,7 +48,7 @@ const DoctorAppointment = () => {
                   </svg>
                 </a>
                 <a href="" className='border rounded-lg'>
-                  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <svg className='w-3' viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M6.288 12.5331L11.088 7.73311L6.288 2.93311L4.928 4.29311L8.352 7.73311L4.912 11.1731L6.288 12.5331Z" fill="#A6FFF2" />
                   </svg>
                 </a>
