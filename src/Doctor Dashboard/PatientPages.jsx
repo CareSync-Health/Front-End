b241e9ce679437ff873ce3dc7cb9ed42036a7bf5
Jephthah Ndukwe <jephthahndukwe@gmail.com>
2024-07-01 20:09:57 +0100

    new update

--- src/Doctor Dashboard/PatientPages.jsx
+++ src/Doctor Dashboard/PatientPages.jsx
@@ -13,35 +13,34 @@ const PatientPages = () => {
   return (
     <div className='flex'>
         <Sidebar/>
-      <div className='flex-1 lg:h-[99.9vh]  xs:h-[85vh]  overflow-y-auto bg-[#E2F3F5]' style={{ msOverflowStyle: 'none', scrollbarWidth: 'none' }}>
+      <div className='flex-1 lg:h-[99.9vh] xs:h-[85vh]  overflow-y-auto bg-[#E2F3F5]' style={{ msOverflowStyle: 'none', scrollbarWidth: 'none' }}>
         <Navbar messageCount={5} notificationCount={12}/>
         <div>
           {/* starting coding from here don't touch any other thing from the navbar and sidebar please. if you touch am... YOU DIE 🔪😤 */}
           
           {/*FILTER BOX */}
 
-          <div className='px-[50px] '>
-          <div className=' bg-white w-[190px] h-[40px] rounded-[9px] px-[50px] ml-[77%] mt-[20px] '>
-  
-  <div className=' flex items-center justify-items-center w-[110px] h-[40px] gap-[9px]'>
-    <h1 className='text-[#17B978] text-[14px] font-Inter font-bold leading-[20px] text-justify'>Filter Patient</h1>
-    <RiArrowDropDownLine />
-  </div>
-</div>
+          <div className='lg:px-[50px]  xs:px-[10px]'>
+            <div className='flex justify-end mt-[20px] '>
+    
+              <div className='bg-white w-[190px] h-[40px] flex items-center justify-between rounded-[12px] px-[20px]'>
+                <h1 className='text-[#17B978] text-[14px] font-Inter font-bold leading-[20px]'>Filter Patient</h1>
+                <RiArrowDropDownLine className='text-[22px]' />
+              </div>
+            </div>
  {/*ENDS HERE */}
 
   {/*DOCTOR'S STATS */}
 
-          <div className='flex items-center mt-[2rem] gap-[5rem] ml-[1rem]'>
-            <div className='bg-white w-[250px] h-[88px] rounded-[10px] px-[15px] flex items-center justify-start gap-[1rem]'>
+          <div className='flex items-center justify-between lg:gap-0 xs:gap-[1rem] flex-wrap mt-[2rem] lg:ms-[1rem]'>
+            <div className='bg-white lg:w-[250px] xs:w-full py-[15px] rounded-[10px] px-[15px] flex items-center justify-start gap-[1rem]'>
               <img src={crutch} />
               <div>
                 <h1 className='text-[#25282B] text-[14px] font-Inter font-bold leading-[20px] text-start'>All Patients</h1>
                 <h2 className='text-[#22D1EE] text-[16px] font-Inter font-normal leading-[24px] text-start mt-[5px]'>0</h2>
               </div>
             </div>
-            <div className='bg-white w-[145px] h-[88px] rounded-[12px] px-[15px] flex items-center justify-center gap-[1rem]'>
-  
+            <div className='bg-white lg:w-[145px] xs:w-full py-[15px] rounded-[12px] px-[20px] flex lg:items-center lg:justify-center gap-[1rem]'>
               <div>
                 <h1 className='text-[#25282B] text-[14px] font-Inter font-bold leading-[20px] text-start'>Online</h1>
                 <div className="flex gap-[0.5rem] mr[3rem]">
@@ -50,8 +49,7 @@ const PatientPages = () => {
                 </div>
               </div>
             </div>
-            <div className='bg-white w-[145px] h-[88px] rounded-[12px] px-[15px] flex items-center justify-center gap-[1rem]'>
-  
+            <div className='bg-white lg:w-[145px] xs:w-full py-[15px] rounded-[12px] px-[20px] flex lg:items-center lg:justify-center xs:items-start xs:justify-start gap-[1rem]'>
               <div>
                 <h1 className='text-[#25282B] text-[14px] font-Inter font-bold leading-[20px] text-start'>Offline</h1>
                 <div className="flex gap-[0.5rem] mr[3rem]">
@@ -60,26 +58,23 @@ const PatientPages = () => {
                 </div>
               </div>
             </div>
-
-
-            <div className='bg-[#F8DEBD] w-[145px] h-[88px] rounded-[12px] px-[15px] flex items-center justify-center gap-[1rem]'>
-  
+            <div className='bg-[#F8DEBD] lg:w-[145px] xs:w-full lg:py-[5px] xs:py-[15px] rounded-[12px] px-[20px] flex g:items-center lg:justify-center gap-[1rem]'>
                 <div>
-               <h1 className='text-[#25282B] text-[14px] font-Inter font-bold leading-[20px] text-center'>Currently attending to</h1>
-             <div className="flex items-center justify-center gap-[0.5rem] ">
+               <h1 className='text-[#25282B] text-[13px] font-Inter font-bold leading-[20px] text-center'>Currently attending to</h1>
+             <div className="flex lg:items-center lg:justify-center gap-[0.5rem] ">
             <h2 className='text-[#000] text-[20px] font-Inter font-normal leading-[24px] text-start mt-[5px]'>0</h2>
               <BsFillHeartPulseFill  className='mt-[8px] fill-red-500' />
                     </div>
                 </div>
            </div>
 
-          <div className='bg-[#D0FBFF] w-[145px] h-[88px] rounded-[12px] px-[15px] flex items-center justify-center gap-[1rem]'>
+          <div className='bg-[#D0FBFF] lg:w-[145px] xs:w-full py-[15px] rounded-[12px] px-[20px] flex g:items-center lg:justify-center gap-[1rem]'>
   
               <div>
-                <h1 className='text-[#25282B] text-[14px] font-Inter font-bold leading-[20px] text-start'>Offline</h1>
-                <div className="flex gap-[0.5rem] mr[3rem]">
-                <h2 className='text-[#000] text-[20px] font-Inter font-normal leading-[24px] text-start mt-[5px]'>0</h2>
-                <BiDownArrowAlt className='mt-[8px] fill-red-500' />
+                <h1 className='text-[#25282B] text-[14px] font-Inter font-bold leading-[20px] text-center'>Completed attended</h1>
+                <div className="flex gap-[0.5rem] lg:ms-[2rem]">
+                  <h2 className='text-[#000] text-[20px] font-Inter font-normal leading-[24px] mt-[5px]'>0</h2>
+                  <BiDownArrowAlt className='mt-[8px] fill-red-500' />
                 </div>
               </div>
             </div>
@@ -94,46 +89,45 @@ const PatientPages = () => {
             {/*TABLE PART */}
             
           </div>
-          <div className='mt-[3rem] ml-[1.9rem]'>
-          <h1 className='text-[#25282B] text-[2rem] font-Inter font-bold leading-[10px]'> Patient</h1>
-        <h1 className='text-[#25282B] text-[2rem] font-Inter font-bold leading-[35px] mt-[3rem]'>No any attending patient yet, once you start attending to <br></br> any patient, they will appear in the table below </h1>
+          <div className='mt-[5rem] lg:ms-[3.5rem] px-[10px]'>
+            <h1 className='text-[#25282B] text-[2rem] font-Inter font-bold leading-[10px]'> Patient</h1>
+            <h1 className='text-[#25282B] lg:w-[1000px] lg:text-[1.5rem] xs:text-[1.2rem] font-Inter font-bold leading-[35px] mt-[3rem]'>No any attending patient yet, once you start attending to any patient, they will appear in the table below </h1>
           </div>
-          <div className='  bg-white w-[85%] h-[15rem] ml-[1.9rem] mt-[2rem]  rounded-[10px]'>
-          <table className=" w-full table-auto">
-  <thead className='bg-gray-50 border-b-2 border-gray-200'>
-    <tr>
-      <th className='p-3 text-sm font-semibold tracking-wide text-left'>Patient Name</th>
-      <th className='p-3 text-sm font-semibold tracking-wide text-left'>Email</th>
-      <th className='p-3 text-sm font-semibold tracking-wide text-left'>Inbox</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr className='bg-white border-b-2'>
-      <td className='p-4 text-sm text-gray-700 '></td>
-      <td className='p-4 text-sm text-gray-700 '></td>
-      <td className='p-4 text-sm text-gray-700 '></td>
-    </tr>
-    <tr className='bg-white border-b-2'>
-      <td className='p-4 text-sm text-gray-700 '></td>
-      <td className='p-4 text-sm text-gray-700 '></td>
-      <td className='p-4 text-sm text-gray-700 '></td>
-    </tr>
-    <tr className='bg-white border-b-2'>
-      <td className='p-4 text-sm text-gray-700 '></td>
-      <td className='p-4 text-sm text-gray-700 '></td>
-      <td className='p-4 text-sm text-gray-700 '></td>
-    </tr>
-    <tr className='bg-white border-b-2'>
-      <td className='p-4 text-sm text-gray-700 '></td>
-      <td className='p-4 text-sm text-gray-700 '></td>
-      <td className='p-4 text-sm text-gray-700 '></td>
-    </tr>
-    
-  </tbody>
-</table>
-   <button className= 'bg-[#17B978] w-[60px] h-[60px] rounded-full ml-[72rem] mt-[1rem] fixed'><FaUserDoctor className='ml-[1.4rem]' /></button>
+          <div className='bg-white lg:w-[90%] xs:w-[95%] lg:ms-[4rem] xs:ms-[0.5rem] mt-[3rem] rounded-[10px] mb-[3rem]'>
+            <table className=" w-full table-auto">
+              <thead className='bg-gray-50 border-b-2 border-gray-200'>
+                <tr>
+                  <th className='px-4 py-3 text-sm font-semibold tracking-wide text-left'>Patient Name</th>
+                  <th className='px-4 py-3 text-sm font-semibold tracking-wide text-left'>Email</th>
+                  <th className='px-4 py-3 text-sm font-semibold tracking-wide'>Inbox</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className='bg-white border-b-2'>
+                  <td className='p-4 text-sm text-gray-700 '></td>
+                  <td className='p-4 text-sm text-gray-700 '></td>
+                  <td className='p-4 text-sm text-gray-700 '></td>
+                </tr>
+                <tr className='bg-white border-b-2'>
+                  <td className='p-4 text-sm text-gray-700 '></td>
+                  <td className='p-4 text-sm text-gray-700 '></td>
+                  <td className='p-4 text-sm text-gray-700 '></td>
+                </tr>
+                <tr className='bg-white border-b-2'>
+                  <td className='p-4 text-sm text-gray-700 '></td>
+                  <td className='p-4 text-sm text-gray-700 '></td>
+                  <td className='p-4 text-sm text-gray-700 '></td>
+                </tr>
+                <tr className='bg-white border-b-2'>
+                  <td className='p-4 text-sm text-gray-700 '></td>
+                  <td className='p-4 text-sm text-gray-700 '></td>
+                  <td className='p-4 text-sm text-gray-700 '></td>
+                </tr>
+              </tbody>
+            </table>
+            <button className= 'bg-[#17B978] w-[60px] h-[60px] rounded-full ml-[72rem] mt-[1rem] fixed'><FaUserDoctor className='ml-[1.4rem]' /></button>
+          </div>
     {/*ENDS HERE */}
-          </div>
        
         </div>
 
