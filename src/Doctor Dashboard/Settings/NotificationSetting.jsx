120e71010b8d6b0cb0f558c1cdf11e506d44b8ca
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-13 00:12:46 +0100

    auto-commit

--- src/Doctor Dashboard/Settings/NotificationSetting.jsx
+++ src/Doctor Dashboard/Settings/NotificationSetting.jsx
@@ -12,7 +12,7 @@ const [check, setCheck] = useState(false)
           <p className='text-xl mt-8'>When CareSyn web page is closed continue to show notifications</p>
           <label class="switch">
             <div class="toggle-switch relative w-[80px] h-[40px]">
-              <input id="switch" type="checkbox" className='hidden group'/>
+              <input checked={check} onChange={()=>setCheck(!check)} id="switch" type="checkbox" className='hidden group'/>
               <label style={{background:"linear-gradient(to right, #2e2e2e, #4a4a4a)", transition:"background 0.3s"}} for="switch" className='absolute group-checked: cursor-pointer top-0 left-0 right-0 bottom-0'></label>
             </div>
           </label>
