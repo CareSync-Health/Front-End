e67ca1a18f4c8806acd6045f2a4fad87f7d13da2
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-13 00:35:59 +0100

    auto-commit

--- src/Doctor Dashboard/Settings/NotificationSetting.jsx
+++ src/Doctor Dashboard/Settings/NotificationSetting.jsx
@@ -10,12 +10,29 @@ const [check, setCheck] = useState(false)
         <p className='font-medium text-xl mt-9'>When CareSync is Closed</p>
         <span>
           <p className='text-xl mt-8'>When CareSyn web page is closed continue to show notifications</p>
-          <label class="switch">
-            <div class="toggle-switch relative w-[80px] h-[40px]">
-              <input checked={check} onChange={()=>setCheck(!check)} id="switch" type="checkbox" className='hidden'/>
-              <label style={{background:`${check ? "linear-gradient(to right, #66bb6a, #43a047)" : 'linear-gradient(to right, #2e2e2e, #4a4a4a)'}`, transition:"background 0.3s"}} for="switch" className={`${check ? "before:translate-x-[40px] before:bg-[#66bb6a]" : ""}absolute cursor-pointer before:absolute before:h-[36px] before:shadow-md before:duration-300 before:transition-all  before:w-[36px] before:left-[2px] before:bg-[#fff] before:-bottom-[2px] before:content-none top-0 left-0 right-0 bottom-0`}></label>
-            </div>
-          </label>
+          <label className="switch">
+      <div className="toggle-switch relative w-[80px] h-[40px]">
+        <input
+          checked={check}
+          onChange={() => setCheck(!check)}
+          id="switch"
+          type="checkbox"
+          className="hidden"
+        />
+        <label
+          style={{
+            background: check
+              ? 'linear-gradient(to right, #66bb6a, #43a047)'
+              : 'linear-gradient(to right, #2e2e2e, #4a4a4a)',
+            transition: 'background 0.3s',
+          }}
+          htmlFor="switch"
+          className={`absolute top-0 left-0 right-0 bottom-0 cursor-pointer rounded-full transition-all duration-300
+            before:absolute before:h-[36px] before:w-[36px] before:left-[2px] before:bottom-[2px] before:rounded-full before:bg-[#fff] before:shadow-md before:transition-all before:duration-300
+            ${check ? 'before:translate-x-[40px] before:bg-[#66bb6a]' : ''}`}
+        ></label>
+      </div>
+    </label>
         </span>
       </div>
     </div>
