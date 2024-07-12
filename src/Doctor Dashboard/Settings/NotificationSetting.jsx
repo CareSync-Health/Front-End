6608ffd60a1077e5606dac317a908ca964c71550
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-12 23:51:30 +0100

    auto-commit

--- src/Doctor Dashboard/Settings/NotificationSetting.jsx
+++ src/Doctor Dashboard/Settings/NotificationSetting.jsx
@@ -7,7 +7,7 @@ const NotificationSetting = () => {
   return (
     <div>
          <div className={`${appearance === 'green' ? 'text-[#17B978]' : appearance === 'blue' ? 'text-[#22D1EE]' : appearance === 'accent' ? 'text-[#A6FFF2]' : theme === 'dark' ? 'text-white' : 'text-gray-800'}`} style={{ msOverflowStyle: 'none', scrollbarWidth: 'none' }}>
-            <p className='font-medium text-xl mt-4'>When CareSync is Closed</p>
+            <p className='font-medium text-xl mt-5'>When CareSync is Closed</p>
         </div>
     </div>
   )
