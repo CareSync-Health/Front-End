31a93f373b02c72a96988ca689d8baa218c0c688
Aniekwensicodex <aniekwensiemmanuel59@gmail.com>
2024-07-12 15:13:30 +0100

    new commit

--- src/Patient Dashboard/Message/PatientMessage.jsx
+++ src/Patient Dashboard/Message/PatientMessage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Sidebar from '../Components/Sidebar'
 import Navbar from '../Components/Navbar'
 import MiniNavbar from '../Components/MiniNavbar'
+import { IoOptions } from "react-icons/io5";
 
 const PatientMessage = () => {
   return (
@@ -10,9 +11,12 @@ const PatientMessage = () => {
         <div className='flex-1 lg:h-[99.9vh] xs:h-[85vh] overflow-y-auto bg-[#FFFCF8]' style={{ msOverflowStyle: 'none', scrollbarWidth: 'none' }}>
             <Navbar/>
             <MiniNavbar/>
-            <div>
-              Patient Message
+            <div className='w-[95rem] flex items-center justify-between mt-[4rem] ml-[2rem]'>
+              <h1 className='text-[30px] font-Inter font-bold leading-[20px] text-start'>Messages</h1>
+              <IoOptions  className='w-[10rem] h-[3rem]'/>
             </div>
+            <hr className='w-[91.5rem] h-[4px] bg-#94949480 mt-[3rem] ml-[2rem]' />
+            <h2 className='text-[30px] font-Inter font-bold leading-[50px] text-start mt-[4rem] ml-[2rem]'>No any message yet, once you have an unread <br />message, it will display here </h2>
         </div>
     </div>
   )
