f70760b9f751376a9dd30c414b79017da8d64496
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-13 00:12:08 +0100

    auto-commit

--- src/Doctor Dashboard/Settings/NotificationSetting.jsx
+++ src/Doctor Dashboard/Settings/NotificationSetting.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useTheme } from '../Components/ThemeContext'
 
 const NotificationSetting = () => {
   const { theme, appearance } = useTheme();
-const [check, setCheck] = use
+const [check, setCheck] = useState(false)
   return (
     <div>
       <div className={`${appearance === 'green' ? 'text-[#17B978]' : appearance === 'blue' ? 'text-[#22D1EE]' : appearance === 'accent' ? 'text-[#A6FFF2]' : theme === 'dark' ? 'text-white' : 'text-gray-800'}`} style={{ msOverflowStyle: 'none', scrollbarWidth: 'none' }}>
