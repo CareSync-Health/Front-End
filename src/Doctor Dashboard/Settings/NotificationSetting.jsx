129cffddda627ed0ae7dfb1155319e8493c0f3c1
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-13 00:02:17 +0100

    auto-commit

--- src/Doctor Dashboard/Settings/NotificationSetting.jsx
+++ src/Doctor Dashboard/Settings/NotificationSetting.jsx
@@ -11,7 +11,7 @@ const NotificationSetting = () => {
         <span>
           <p className='text-xl mt-8'>When CareSyn web page is closed continue to show notifications</p>
           <label class="switch">
-            <div class="toggle-switch relative">
+            <div class="toggle-switch relative w-[80px] h-[40px]">
               <input id="switch" type="checkbox" />
               <label for="switch"></label>
             </div>
