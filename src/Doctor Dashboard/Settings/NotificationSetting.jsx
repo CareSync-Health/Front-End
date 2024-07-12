65586862a32e3973d77942aed0da2a28ad3caff2
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-13 00:01:28 +0100

    auto-commit

--- src/Doctor Dashboard/Settings/NotificationSetting.jsx
+++ src/Doctor Dashboard/Settings/NotificationSetting.jsx
@@ -6,13 +6,18 @@ const NotificationSetting = () => {
 
   return (
     <div>
-         <div className={`${appearance === 'green' ? 'text-[#17B978]' : appearance === 'blue' ? 'text-[#22D1EE]' : appearance === 'accent' ? 'text-[#A6FFF2]' : theme === 'dark' ? 'text-white' : 'text-gray-800'}`} style={{ msOverflowStyle: 'none', scrollbarWidth: 'none' }}>
-            <p className='font-medium text-xl mt-9'>When CareSync is Closed</p>
-            <span>
-              <p className='text-xl mt-8'>When CareSyn web page is closed continue to show notifications</p>
-
-            </span>
-        </div>
+      <div className={`${appearance === 'green' ? 'text-[#17B978]' : appearance === 'blue' ? 'text-[#22D1EE]' : appearance === 'accent' ? 'text-[#A6FFF2]' : theme === 'dark' ? 'text-white' : 'text-gray-800'}`} style={{ msOverflowStyle: 'none', scrollbarWidth: 'none' }}>
+        <p className='font-medium text-xl mt-9'>When CareSync is Closed</p>
+        <span>
+          <p className='text-xl mt-8'>When CareSyn web page is closed continue to show notifications</p>
+          <label class="switch">
+            <div class="toggle-switch">
+              <input id="switch" type="checkbox" />
+              <label for="switch"></label>
+            </div>
+          </label>
+        </span>
+      </div>
     </div>
   )
 }
