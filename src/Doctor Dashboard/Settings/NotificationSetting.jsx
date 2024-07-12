56d779d414d59e255f1b451c1285adf972fe725c
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-13 00:58:12 +0100

    auto-commit

--- src/Doctor Dashboard/Settings/NotificationSetting.jsx
+++ src/Doctor Dashboard/Settings/NotificationSetting.jsx
@@ -8,8 +8,8 @@ const NotificationSetting = () => {
     <div>
       <div className={`${appearance === 'green' ? 'text-[#17B978]' : appearance === 'blue' ? 'text-[#22D1EE]' : appearance === 'accent' ? 'text-[#A6FFF2]' : theme === 'dark' ? 'text-white' : 'text-gray-800'}`} style={{ msOverflowStyle: 'none', scrollbarWidth: 'none' }}>
         <p className='font-medium text-xl mt-9'>When CareSync is Closed</p>
-        <span className='flex justify-between items-center bg-black'>
-          <p className='text-xl mt-8'>When CareSyn web page is closed continue to show notifications</p>
+        <span className='flex mt-8 justify-between items-center bg-black'>
+          <p className='text-xl'>When CareSyn web page is closed continue to show notifications</p>
           <label className="switch">
             <div className="toggle-switch relative w-[4rem] h-[1.8rem]">
               <input
