1c0f3989506b9e2679dc35f4958272e9946c0dbb
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-13 00:36:39 +0100

    auto-commit

--- src/Doctor Dashboard/Settings/NotificationSetting.jsx
+++ src/Doctor Dashboard/Settings/NotificationSetting.jsx
@@ -3,7 +3,7 @@ import { useTheme } from '../Components/ThemeContext'
 
 const NotificationSetting = () => {
   const { theme, appearance } = useTheme();
-const [check, setCheck] = useState(false)
+  const [check, setCheck] = useState(false)
   return (
     <div>
       <div className={`${appearance === 'green' ? 'text-[#17B978]' : appearance === 'blue' ? 'text-[#22D1EE]' : appearance === 'accent' ? 'text-[#A6FFF2]' : theme === 'dark' ? 'text-white' : 'text-gray-800'}`} style={{ msOverflowStyle: 'none', scrollbarWidth: 'none' }}>
@@ -11,28 +11,28 @@ const [check, setCheck] = useState(false)
         <span>
           <p className='text-xl mt-8'>When CareSyn web page is closed continue to show notifications</p>
           <label className="switch">
-      <div className="toggle-switch relative w-[80px] h-[40px]">
-        <input
-          checked={check}
-          onChange={() => setCheck(!check)}
-          id="switch"
-          type="checkbox"
-          className="hidden"
-        />
-        <label
-          style={{
-            background: check
-              ? 'linear-gradient(to right, #66bb6a, #43a047)'
-              : 'linear-gradient(to right, #2e2e2e, #4a4a4a)',
-            transition: 'background 0.3s',
-          }}
-          htmlFor="switch"
-          className={`absolute top-0 left-0 right-0 bottom-0 cursor-pointer rounded-full transition-all duration-300
+            <div className="toggle-switch relative w-[80px] h-[40px]">
+              <input
+                checked={check}
+                onChange={() => setCheck(!check)}
+                id="switch"
+                type="checkbox"
+                className="hidden"
+              />
+              <label
+                style={{
+                  background: check
+                    ? 'linear-gradient(to right, #66bb6a, #43a047)'
+                    : 'linear-gradient(to right, #2e2e2e, #4a4a4a)',
+                  transition: 'background 0.3s',
+                }}
+                htmlFor="switch"
+                className={`absolute top-0 left-0 right-0 bottom-0 cursor-pointer rounded-full transition-all duration-300
             before:absolute before:h-[36px] before:w-[36px] before:left-[2px] before:bottom-[2px] before:rounded-full before:bg-[#fff] before:shadow-md before:transition-all before:duration-300
             ${check ? 'before:translate-x-[40px] before:bg-[#66bb6a]' : ''}`}
-        ></label>
-      </div>
-    </label>
+              ></label>
+            </div>
+          </label>
         </span>
       </div>
     </div>
