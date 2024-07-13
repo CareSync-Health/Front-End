c41ad5bf3fff78ad400e7121695f3afa1388138b
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-13 01:18:41 +0100

    auto-commit

--- src/Doctor Dashboard/Settings/NotificationSetting.jsx
+++ src/Doctor Dashboard/Settings/NotificationSetting.jsx
@@ -75,10 +75,13 @@ const NotificationSetting = () => {
         <p className='font-medium text-xl mt-9'>Notification Sound</p>
         <div>
           <p className='text-xl'>Message</p>
-          <span className='text-xl'>
+          <span className='text-xl flex items-center'>
             <img className='w-14' src={play} alt="" />
             Play Sound
           </span>
+          <select name="" id="">
+            <option value=""></option>
+          </select>
         </div>
       </div>
     </div>
