0837148f2c4f056a18b77ff2c38777b0f02be3a2
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-13 01:23:47 +0100

    auto-commit

--- src/Doctor Dashboard/Settings/NotificationSetting.jsx
+++ src/Doctor Dashboard/Settings/NotificationSetting.jsx
@@ -73,7 +73,7 @@ const NotificationSetting = () => {
           </label>
         </span>
         <p className='font-medium text-xl mt-9'>Notification Sound</p>
-        <div className='flex justify-between items-center'>
+        <div className='flex justify-between items-center mt-6'>
           <p className='text-xl w-[25%] pl-5'>Message</p>
           <span className='text-xl flex gap-2 items-center'>
             <img className='w-14' src={play} alt="" />
