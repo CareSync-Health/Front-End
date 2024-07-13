788d53c003f1cb304725e9b75aaa00022ad203cb
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-14 00:52:36 +0100

    auto-commit

--- src/Patient Dashboard/Settings/PatientSettings.jsx
+++ src/Patient Dashboard/Settings/PatientSettings.jsx
@@ -9,7 +9,7 @@ const PatientSettings = () => {
       <div className='flex-1 lg:h-[99.9vh] xs:h-[85vh] overflow-y-auto bg-[#FFFCF8]' style={{ msOverflowStyle: 'none', scrollbarWidth: 'none' }}>
         <section className='px-7'>
           <div className='flex justify-between flex-wrap items-center'>
-            <span className='flex items-center  gap-10'>
+            <span className='flex items-center flex-wrap gap-10'>
               <img src={avatar} alt="" className='rounded-full w-32 h-32 border-[#E2F3F5] border-[6px]' />
               <span >
                 <p className='text-[#384D6C] text-lg font-bold'>Ay Tech, Abdul</p>
@@ -17,7 +17,7 @@ const PatientSettings = () => {
                 <p className='text-[#384D6C] text-sm'>{"Eastern European Time (EET), Cairo UTC +3"}</p>
               </span>
             </span>
-            <span className='flex gap-5 items-center'>
+            <span className='flex gap-5 flex-wrap items-center'>
               <a className='px-4 font-bold text-white text-xs py-3 bg-[#22D1EE] border border-[#A6FFF2] rounded-lg'>Upload New Photo</a>
               <a className='px-14 font-bold text-[#384D6C] text-xs py-3 bg-[#E2F3F5] border border-[#384D6C] rounded-lg'>Delete</a>
             </span>
