4e33a0415f722430ba6127d17b66a922db9f29a1
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-14 00:57:49 +0100

    auto-commit

--- src/Patient Dashboard/Settings/PatientSettings.jsx
+++ src/Patient Dashboard/Settings/PatientSettings.jsx
@@ -10,8 +10,8 @@ const PatientSettings = () => {
         <section className='md:px-7 px-4'>
           <div className='flex justify-between flex-wrap items-center'>
             <span className='flex items-center gap-5 md:gap-10'>
-              <img src={avatar} alt="" className='rounded-full md:w-32 md:h-32 w-24 h-24 border-[#E2F3F5] border-[6px]' />
-              <span >
+              <img src={avatar} alt="" className='rounded-full mt-4 md:w-32 md:h-32 w-24 h-24 border-[#E2F3F5] border-[6px]' />
+              <span className='mt-4'>
                 <p className='text-[#384D6C] text-lg font-bold'>Ay Tech, Abdul</p>
                 <p className='text-[#384D6C] text-lg'>Product Design</p>
                 <p className='text-[#384D6C] text-sm'>Eastern European Time (EET), Cairo UTC +3</p>
