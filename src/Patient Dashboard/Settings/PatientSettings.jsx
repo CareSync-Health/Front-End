2f44d863ca446efab590a7717468d9627b2b6cd2
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-14 00:39:14 +0100

    auto-commit

--- src/Patient Dashboard/Settings/PatientSettings.jsx
+++ src/Patient Dashboard/Settings/PatientSettings.jsx
@@ -7,7 +7,7 @@ const PatientSettings = () => {
     <div className='flex'>
       <Sidebar />
       <div className='flex-1 lg:h-[99.9vh] xs:h-[85vh] overflow-y-auto bg-[#FFFCF8]' style={{ msOverflowStyle: 'none', scrollbarWidth: 'none' }}>
-        <section>
+        <section className='px-7'>
           <div>
             <span className='flex items-center gap-10'>
               <img src={avatar} alt="" className='rounded-full w-32 h-32 border-[#E2F3F5] border-[6px]'/>
