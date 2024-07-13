4c7c72a397d38d68ea2cd49354cec548e54b83da
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-14 00:50:14 +0100

    auto-commit

--- src/Patient Dashboard/Settings/PatientSettings.jsx
+++ src/Patient Dashboard/Settings/PatientSettings.jsx
@@ -19,7 +19,7 @@ const PatientSettings = () => {
             </span>
             <span>
               <a className='px-4 font-bold text-white text-xs py-3 bg-[#22D1EE] border border-[#A6FFF2] rounded-lg'>Upload New Photo</a>
-              <a className='px-4 font-bold text-[#384D6C] text-xs py-3 bg-[#E2F3F5] border border-[#384D6C] rounded-lg'>Delete</a>
+              <a className='px-8 font-bold text-[#384D6C] text-xs py-3 bg-[#E2F3F5] border border-[#384D6C] rounded-lg'>Delete</a>
             </span>
           </div>
         </section>
