09b9cf9441403f47b8ed73b0fbcd03bf8b8b73bd
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-21 21:11:55 +0100

    auto-commit

--- src/Patient Dashboard/Settings/PatientSettings.jsx
+++ src/Patient Dashboard/Settings/PatientSettings.jsx
@@ -38,6 +38,16 @@ const PatientSettings = () => {
               <input className='border outline-none placeholder:italic placeholder:font-light border-[#D1D5DB] px-5 py-3 rounded-lg mt-3' type="text" placeholder='eg. alaa.mohamed'/>
             </div>
           </div>
+          <div className='mt-12 flex md:px-12 md:gap-9 flex-wrap md:flex-nowrap'>
+            <div className='flex flex-col w-full md:w-1/2 mt-3'>
+              <label className='text-[0.87rem] font-bold text-[#384D6C]' htmlFor="">First Name</label>
+              <input className='border outline-none placeholder:italic placeholder:font-light border-[#D1D5DB] px-5 py-3 rounded-lg mt-3' type="text" placeholder='e.g Alaa'/>
+            </div>
+            <div className='flex flex-col w-full md:w-1/2 mt-3'>
+              <label className='text-[0.87rem] font-bold text-[#384D6C]' htmlFor="">Last Name</label>
+              <input className='border outline-none placeholder:italic placeholder:font-light border-[#D1D5DB] px-5 py-3 rounded-lg mt-3' type="text" placeholder='eg. Mohamed'/>
+            </div>
+          </div>
         </section>
       </div>
     </div>
