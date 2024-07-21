95902d38cbb2337f81c193e29c5356b8b06e2e63
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-21 20:52:58 +0100

    auto-commit

--- src/Patient Dashboard/Settings/PatientSettings.jsx
+++ src/Patient Dashboard/Settings/PatientSettings.jsx
@@ -29,7 +29,7 @@ const PatientSettings = () => {
             </div>
             <div className='flex flex-col w-1/2'>
               <label className='text-[0.87rem] font-bold text-[#384D6C]' htmlFor="">Last Name</label>
-              <input className='border border-[#D1D5DB] px-5 py-3 rounded-lg mt-3' type="text" placeholder='e.g Alaa'/>
+              <input className='border border-[#D1D5DB] px-5 py-3 rounded-lg mt-3' type="text" placeholder='eg. Mohamed'/>
             </div>
           </div>
         </section>
