4e4f16f7ee46a678dfef33a491f8c055e272152e
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-21 21:44:44 +0100

    auto-commit

--- src/Patient Dashboard/Settings/PatientSettings.jsx
+++ src/Patient Dashboard/Settings/PatientSettings.jsx
@@ -46,7 +46,7 @@ const PatientSettings = () => {
           <div className='mt-10 border-b pb-9 flex md:px-12 px-2 md:gap-9 flex-wrap md:flex-nowrap'>
             <div className='flex flex-col w-full md:w-1/2 mt-3'>
               <label className='text-[0.87rem] font-bold text-[#384D6C]' htmlFor="">Email Address</label>
-              <input  className='border ll outline-none placeholder:italic placeholder:font-light border-[#D1D5DB] px-5 py-3 rounded-lg mt-3' type="text" placeholder=''/>
+              <input value={email} onChange={(e)=> setEmail(e.target.value)} className='border ll outline-none placeholder:italic placeholder:font-light border-[#D1D5DB] px-5 py-3 rounded-lg mt-3' type="text" placeholder=''/>
             </div>
             <div className='flex flex-col w-full md:w-1/2 mt-3'>
               <label className='text-[0.87rem] font-bold text-[#384D6C]' htmlFor="">Phone Number</label>
