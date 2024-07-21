a8594824a30313e0924b209937f3014eac484999
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-21 21:03:18 +0100

    auto-commit

--- src/Patient Dashboard/Settings/PatientSettings.jsx
+++ src/Patient Dashboard/Settings/PatientSettings.jsx
@@ -32,7 +32,7 @@ const PatientSettings = () => {
               <input className='border placeholder:italic placeholder:font-light border-[#D1D5DB] px-5 py-3 rounded-lg mt-3' type="text" placeholder='eg. Mohamed'/>
             </div>
           </div>
-          <div className='mt-3 flex md:px-12 gap-9 flex-wrap md:flex-nowrap'>
+          <div className='m3 flex md:px-12 gap-9 flex-wrap md:flex-nowrap'>
             <div className='flex flex-col w-full'>
               <label className='text-[0.87rem] font-bold text-[#384D6C]' htmlFor="">First Name</label>
               <input className='border placeholder:italic placeholder:font-light border-[#D1D5DB] px-5 py-3 rounded-lg mt-3' type="text" placeholder='e.g Alaa'/>
