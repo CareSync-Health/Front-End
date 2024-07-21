c21807d79157638d8582aa9d2914a7ad9dfe60cf
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-21 20:17:02 +0100

    auto-commit

--- src/Patient Dashboard/Settings/PatientSettings.jsx
+++ src/Patient Dashboard/Settings/PatientSettings.jsx
@@ -24,7 +24,7 @@ const PatientSettings = () => {
           </div>
           <div>
             <div>
-              <label className='flex fle' htmlFor="">
+              <label className='flex flex-col' htmlFor="">
                 First Name
                 <input type="text" />
               </label>
