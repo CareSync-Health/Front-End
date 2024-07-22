114dfa49162e0a07ae15d0ea7da0abc8a863c471
Jephthah Ndukwe <jephthahndukwe@gmail.com>
2024-07-22 20:35:16 +0100

    new update

--- src/Patient Dashboard/Settings/PatientPayment.jsx
+++ src/Patient Dashboard/Settings/PatientPayment.jsx
@@ -4,8 +4,8 @@ const PatientPayment = () => {
     return (
         <div className=''>
             <div className='flex-1 lg:h-[99.9vh] xs:h-[85vh] overflow-y-auto bg-[#FFFCF8]' style={{ msOverflowStyle: 'none', scrollbarWidth: 'none' }}>
-                <div>
-                    PatientPayment
+                <div className='px-[30px] mt-[4rem]'>
+                    Patient Payment
                 </div>
             </div>
         </div>
