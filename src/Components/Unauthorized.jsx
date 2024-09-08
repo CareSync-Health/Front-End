2fbba31d207f69a182359e963f93dcee6fc32074
Jephthah Ndukwe <jephthahndukwe@gmail.com>
2024-09-08 03:37:10 +0100

    new update

--- src/Components/Unauthorized.jsx
+++ src/Components/Unauthorized.jsx
@@ -3,13 +3,13 @@ import error from '../assets/404.png'
 
 const Unauthorized = () => {
     return (
-        <div className='flex items-center justify-center h-screen'>
+        <div className='flex items-center justify-center h-screen px-[10px]'>
             <div>
                 <div className='flex items-center justify-center'>
                 <img src={error} alt="error" className='w-[300px]' />
                 </div>
-                <h1 className='text-[35px] font-Mulish text-center font-medium mt-4'>Oops, looks like you got lost.</h1>
-                <p className='text-[22px] font-Mulish font-normal text-center mt-1'>You do not have access to this page.</p>
+                <h1 className='lg:text-[35px] xs:text-[25px] md:text-[30px] font-Mulish text-center font-medium mt-4'>Oops, looks like you got lost.</h1>
+                <p className='lg:text-[22px] xs:text-[18px] md:text-[20px] font-Mulish font-normal text-center mt-1'>You do not have access to this page.</p>
             </div>
         </div>
     );
