3bb953b38b0f9f7cec56ade19454a92add397915
Jephthah Ndukwe <jephthahndukwe@gmail.com>
2024-09-14 00:47:55 +0100

    new update

--- src/Redux/Config.js
+++ src/Redux/Config.js
@@ -1,7 +1,7 @@
 const config = {
-    // liveUrl: "https://back-end-e5dm.onrender.com/api",
+    liveUrl: "https://back-end-e5dm.onrender.com/api",
     socketUrl: "https://back-end-e5dm.onrender.com",
-    liveUrl: "http://localhost:5000/api",
+    // liveUrl: "http://localhost:5000/api",
     // socketUrl: "http://localhost:5000"
 }
 
